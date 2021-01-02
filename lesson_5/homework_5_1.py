a9cffb94cdd8db10bef33ce33ee60aca6b5e7e04
MarinaAz <mariaziz69@gmail.com>
2021-01-02 17:10:55 -0500

    homework_5_1.py

--- lesson_5/homework_5_1.py
+++ lesson_5/homework_5_1.py
@@ -3,29 +3,44 @@
 # Difference
 # Write a function, which will calculate the difference of these two numbers
 
-def difference(num_1, num_2):
-    pass
+num_1 = 5
+num_2 = 3
+def dif_two_numbers(num_1, num_2):
+   print(num_1 - num_2)
+
+dif_two_numbers(num_1, num_2)
 
 
 # Division
 # Write a function, which will divide these two numbers
 
-def division(num_1, num_2):
-    pass
+ def division(num_1, num_2):
+    print(num_1 / num_2)
+
+division(num_1, num_2)
 
 
 # Function gets random number. If this number is more than ten, return the difference between 100 and this number,
 # otherwise return this number multiplied by 10
 
-def function_1(number):
-    pass
+
+def function_1(number: int):
+    if number > 10:
+        return 100 - number
+    else:
+        return number * 10
+
 
 
 # Your function temerature_convertor gets the temperature in Fahrenheit, convert it to Celsius and return.
 # Formula (32°F − 32) × 5/9 = 0°C
 
-def temerature_convertor(fahrenheit_degree):
-    pass
+def temperature_convertor(fahrenheit_degree: int):
+    return round((fahrenheit_degree - 32) * 5/9, 2)
+
+temperature_fahrenheit = int(input('What temperature is right now?: ' ))
+print(temerature_convertor(temperature_fahrenheit))
+
 
 
 # Taxi Fare
@@ -34,7 +49,9 @@ def temerature_convertor(fahrenheit_degree):
 # as its only result rounded by 2 digits. Write a program that demonstrates the function.
 
 def taxi_fare(distance):
-    pass
+    return round(4.0 + ((distance * 1000) / 140) * 0.25, 2)
+
+print(taxi_fare(10))
 
 # examples of usage:
 # taxi_fare(10) #21.86
